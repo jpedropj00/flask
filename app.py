893ff6c0516bc297b1b6b1d8835eb66f64218f85
joaopedrods1 <joaopedrosouzarodriguesj@gmail.com>
2025-11-23 17:40:20 -0300

    adicionando bd

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(100), nullable = False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 # Definindo rota(página principal) a requisição
 @app.route('/teste')
 def hello_world():
