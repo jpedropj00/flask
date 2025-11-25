8786e5bf138dda8b6ade66e88a42df26314fb024
joaopedrods1 <joaopedrosouzarodriguesj@gmail.com>
2025-11-25 20:19:50 -0300

    comitando novas funcionalidades da API

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
@@ -15,7 +14,45 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
 @app.route('/api/products/add', methods=['POST'])
 def add_product():
+        data = request.json
+        if 'name' in data and 'price' in data: 
+            product = Product(name=data['name'], price=data['price'], description= data.get("description", ""))
+            db.session.add(product)
+            db.session.commit()
+            return jsonify({"message" : "Produto cadastrado com sucesso"}), 201
+        return jsonify({"message" : "Dados do produto inválido"}), 400
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    produt = Product.query.get(product_id)
+    if produt:
+        db.session.delete(produt)
+        db.session.commit()
+        return jsonify({'message': 'Produto deletado com sucesso!'}), 200
+    return jsonify({'message': 'Produto não encontrado'}), 404
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product=Product.query.get(product_id)
+    if product:
+        return jsonify({
+            'id': product_id,
+            'name': product.name,
+            'price': product.name,
+            'description': product.description
+        })
+    return jsonify({"message": "Produto não encontrado"}), 404
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product=Product.query.get(product_id)
+    if not product:
+        return jsonify({'message' : 'Produto não encontrado!'}), 404
     data = request.json
-    return data
+    if 'name' in data:
+        product.name = data['name']
+    if 'price' in data:
+        product.price = data['price']
+    if 'description' in data:
+        product.description = data['description']
+    db.session.comit()
+    return jsonify({'message': 'Produto atualizado com sucesso'})
 if __name__ == '__main__':
     app.run(debug=True)
